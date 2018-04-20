6ce3fa2b40eff8469e1aef8a761132066805f409
arpit.sharma <arpit.sharma@pearson.com>
2018-04-20 12:54:54 +0530

    Lottery Contract and Tests

--- compile.js
+++ compile.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 const solc = require('solc');
 
 const inboxPath = path.resolve(__dirname,'contracts','Inbox.sol');
-const source = fs.readFileSync(inboxPath,"utf8");
+const lotteryPath = path.resolve(__dirname,'contracts','Lottery.sol');
 
-module.exports = solc.compile(source,1).contracts[":Inbox"];
+const sourceInbox = fs.readFileSync(inboxPath,"utf8");
+const sourceLottery = fs.readFileSync(lotteryPath,"utf8");
+
+module.exports = {
+    Inbox: solc.compile(sourceInbox,1).contracts[":Inbox"],
+    Lottery: solc.compile(sourceLottery,1).contracts[":Lottery"]
+};
--- deploy.js
+++ deploy.js
@@ -1,6 +1,7 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const {interface, bytecode } = require('./compile');
+const {Inbox, Lottery} =require('./compile');
+
 const config = require('config');
 
 const infuraKey = config.get('infuraKey');
@@ -14,13 +15,24 @@ const provider = new HDWalletProvider(
 );
 const web3 = new Web3(provider);
 
-const deploy = async()=>{
+const deployInbox = async()=>{
     const accounts = await web3.eth.getAccounts();
-    console.log('Attempting to deploy from account',accounts[0]);
+    console.log('Attempting to deploy Inbox Contract from account',accounts[0]);
     
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data:bytecode,arguments:["Hi there!"]})
+    const result = await new web3.eth.Contract(JSON.parse(Inbox.interface))
+        .deploy({data:Inbox.bytecode,arguments:["Hi there!"]})
         .send({from:accounts[0],gas:'1000000'});
-    console.log("Contract deployed to ",result.options.address);
+    console.log("Contract Inbox deployed to ",result.options.address);
 }
-deploy();
+deployInbox();
+
+const deployLottery = async()=>{
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy Lottery from account',accounts[0]);
+    
+    const result = await new web3.eth.Contract(JSON.parse(Lottery.interface))
+        .deploy({data:Lottery.bytecode})
+        .send({from:accounts[0],gas:'1000000'});
+    console.log("Contract Lottery deployed to ",result.options.address);
+}
+deployLottery();
